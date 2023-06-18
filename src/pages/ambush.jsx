46e4377134b61323256de466904e55a6f0b18771
MohamedSaid7102 <mohamedshelfwork@gmail.com>
2023-06-18 19:47:17 +0300

    Cleaned Ambsh Page

--- src/pages/ambush.jsx
+++ src/pages/ambush.jsx
@@ -20,65 +20,19 @@ function Ambush () {
         }
         }
   return (
-  <body>
     <div className="container">
+    {/* Camera Section */} 
       <div className="video-container">
         <div className="frame1">
           <video id="video1" controls width="500" height="500"></video>
-          <br>
-          </br>
-          <form className="FORM1">
-          <div className="pepole"> For People </div>
-
-            <div className="criminalcase">
-              <label className="criminallabel"> Criminal Case :</label>
-              <input className="checkbox" type="checkbox"/>
-            </div>
-            <div className="namediv">
-              <label className="name">Name :</label>
-              <input  className="nameinput" type="text"/>
-              <br></br>
-              <div className='agecontainar'>
-                <label className="age">Age :</label>
-                <input  className="ageinput" type="text"/>
-                <br></br>
-              </div>
-              <div className='cardcriminalcasecontainar'>
-                <label className="card criminal case"> Card Criminal Case :</label>
-                <input  className="cardcriminalcaseinput" type="date"/>
-                <br></br>
-                <br></br>
-                <br></br>
-              </div>
-            </div>
-          </form>
         </div>
         <div className="frame2">
           <video id="video2" controls width="250" height="150"></video>
-          <br></br>
-          <form className="FORM1">
-          <div className="car">    For Car </div>
-
-            <div className="criminalcase">
-              <label className="criminallabel"> Criminal Case :</label>
-              <input className="checkbox" type="checkbox"/>
-              <div className="namediv1">
-                <label className="cardcriminalcar"> license Expiry Date :</label>
-                <input  className="cardcriminalcaseinput" type="date"/>
-                <br></br>
-              </div>
-            </div>
-          </form>
         </div>
       </div>
-      <button className="start-button" onClick={startCamera}>Start Camera</button>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
+    {/* Tabs Section */}
+    <h1>Hello world</h1>
     </div>
-  </body>
 );
 }
 
