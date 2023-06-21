1535cd269b760eb60e06337cae1ac1fb609187d1
MohamedSaid7102 <mohamedshelfwork@gmail.com>
2023-06-21 05:19:58 +0300

    Implemented Start/Stop button & added videos

--- src/pages/ambush.jsx
+++ src/pages/ambush.jsx
@@ -1,41 +1,68 @@
-import React from 'react'; 
+import React, { useRef, useState } from 'react'; 
 import './home.css';
 import TabsSection from './Tabs';
 
 function Ambush () {
-  function startCamera() {
-    // Check if the browser supports getUserMedia
-        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        // Get access to the user's camera
-        navigator.mediaDevices.getUserMedia({ video: true })
-        .then(function (stream) {
-            // Set the stream as the source for both video elements
-            document.getElementById("video1").srcObject = stream;
-            document.getElementById("video2").srcObject = stream;
-        })
-        .catch(function (error) {
-            console.log("Error accessing camera:", error);
-        });
-        } else {
-        console.log("getUserMedia is not supported");
-        }
-        }
+
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handleTogglePlayback = () => {
+    const videos = document.querySelectorAll('video');
+    const newState = !isPlaying;
+
+    videos.forEach((video) => {
+      if (newState) {
+        video.play();
+      } else {
+        video.pause();
+      }
+    });
+
+    setIsPlaying(newState);
+  };
+
+
   return (
     <div className="container">
     {/* Camera Section */} 
-      <div className="video-container">
-        <div className="frame1">
-          <video id="video1" controls width="500" height="500"></video>
-        </div>
-        <div className="frame2">
-          <video id="video2" controls width="250" height="150"></video>
-        </div>
-      </div>
+    <div className="video-container">
+      <VideoPlayer url="https://drive.google.com/uc?export=download&id=11KpFMNgVpzztEvK8DfQBW5629ty0_cmt" />
+      <VideoPlayer url="https://drive.google.com/uc?export=download&id=1x9afw86MD7g5WxDDtjmGJoL-hTbvnOGT" />
+    </div>
+    <button onClick={handleTogglePlayback} id="videosStartButton">{isPlaying ? 'Stop' : 'Start'}</button>
     {/* Tabs Section */}
     <TabsSection />
     </div>
-);
+  );
 }
+
+
+
+const VideoPlayer = ({ url }) => {
+  const videoRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handleTogglePlayback = () => {
+    const video = videoRef.current;
+    if (video.paused) {
+      video.play();
+      setIsPlaying(true);
+    } else {
+      video.pause();
+      setIsPlaying(false);
+    }
+  };
+
+  return (
+    <div>
+    <video ref={videoRef} width="640" height="600" >
+    <source src={url} type="video/mp4" />
+    Your browser does not support the video tag.
+    </video>
+    {/*<button onClick={handleTogglePlayback}>{isPlaying ? 'Stop' : 'Start'}</button>*/}
+    </div>
+  );
+};
 
 export default Ambush;
 
