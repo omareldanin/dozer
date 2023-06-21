27e0106372b2fd7d02aa7700cff85832b8c97c50
MohamedSaid7102 <mohamedshelfwork@gmail.com>
2023-06-21 05:28:49 +0300

    Updated alert button to flash

--- src/pages/AlertsSection.jsx
+++ src/pages/AlertsSection.jsx
@@ -115,12 +115,12 @@ const AlertsSection = ({ carNumber, carLetter, personSSN }) => {
     <div className="w-full p-8 flex flex-row justify-around gap-4">
     {/* Car Section */}
     <div className="flex-1 flex flex-col gap-4 justify-center items-center">
-    <h2>Car</h2>
+    <h2 className="text-3xl">Car</h2>
     <AlertCircle valid={isCarOk} />
     </div>
     {/* Person Section */}
     <div className="flex-1 flex flex-col gap-4 justify-center items-center">
-    <h2>Person</h2>
+    <h2 className="text-3xl">Person</h2>
     <AlertCircle valid={isPersonOk} />
     </div>
     </div>
@@ -128,7 +128,9 @@ const AlertsSection = ({ carNumber, carLetter, personSSN }) => {
 };
 
 const AlertCircle = ({ valid }) => {
-  return <div className={`w-10 h-10 rounded-full ${valid ? 'bg-green-500' : 'bg-red-600'}`}></div>;
+  return (
+    <div className={`w-10 h-10 rounded-full ${valid ? 'bg-green-500' : 'bg-red-600 animate-pulse'}`}></div>
+  );
 };
 
 export default AlertsSection;
